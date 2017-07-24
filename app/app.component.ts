ffc088358127facd31ba3a874ed747f495ff3f10
falent <t.j.krajewski@gmail.com>
2017-07-24 14:30:48 +0200

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'articles',
+    template: '<div>dsfdsffdsf </div>'
+})
+
+export class AppComponent {
+
+}
---
+++ dist/out-tsc/boot.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var browser_1 = require("angular2/platform/browser");
+var app_component_1 = require("./app.component");
+browser_1.bootstrap(app_component_1.AppComponent);
+//# sourceMappingURL=boot.js.map
